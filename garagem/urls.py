72bf96aa62e3b0927afa545f966f6662d1104dec
Thiago Campos <=>
2023-12-27 19:26:59 -0300

    create garagem

---
+++ garagem/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path
+from todos.views import TodoListView, TodoCreateView, TodoHomeView, UpdateHoraAndarView, TudoRelacaoView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TodoHomeView.as_view(), name='home'),
+    path('painel/', TodoListView.as_view(), name='todo_list'),
+    path('adicionar/', TodoCreateView.as_view(), name='todo_create'),
+    path('saida/<str:pk>', UpdateHoraAndarView.as_view(), name='todo_saida'),
+    path('relacao/', TudoRelacaoView.as_view(), name='tudo_relacao'),
+
+]
