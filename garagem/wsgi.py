e3c91e20579ed52625bb67c0c8fa7407c1a077de
Paulo Fernandes Vieira <soeupfv@hotmail.com>
2023-12-27 19:58:27 -0300

    ãtualizaçao nome diretorio

--- garagem/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sistema_Garagem_IFRN.settings')
-
-application = get_wsgi_application()
